7128082c0d618b28c01c0c1223a2c400a92ae963
Lahiri <lahiri.cristofori@gmail.com>
2019-08-18 17:59:25 +0200

    added function help

--- EscapeCharacterToClipboard.ps1
+++ EscapeCharacterToClipboard.ps1
@@ -1,4 +1,19 @@
 function escape {
+    <#
+    .SYNOPSIS
+    Copies the Powershell language escape character ` to the clipboard.
+    
+    .DESCRIPTION
+    Copies the Powershell language escape character ` to the clipboard.
+    This is very useful when using a keyboard that does not have a physical key for the ` character.
+    
+    .EXAMPLE
+    escape
+
+    > Escape character ` copied to clipboard
+    
+    #>
+    
     [CmdletBinding()]
     [alias("esc")]
     param (
